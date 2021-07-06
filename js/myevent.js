9ee002a76c3aa9b4138b372d8929af6c9cd33d7b
byube <kangjunne@naver.com>
2021-07-06 22:45:33 +0900

    프로젝트 수정(0706)

--- js/myevent.js
+++ js/myevent.js
@@ -1,3 +1,15 @@
 $(function(){
     
 });
+
+function goProject(pro){
+    if(pro == 1){
+        window.open("http://www.aiitone.com:20171/");
+    } else if(pro == 2){
+        window.open("https://github.com/Byube/deliveryAPI");
+    } else if(pro == 3){
+        window.open("https://github.com/Byube/Apiisfun");
+    } else if(pro == 4){
+        window.open("https://github.com/Byube/swa/tree/master/swa");
+    }
+}
