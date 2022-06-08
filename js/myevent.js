ef66f5b357e415fc33907003350a48e426d4a17a
jeffkim0619 <jeffkim0619@gmail.com>
2022-06-08 22:52:34 +0900

    프로필 수정

--- js/myevent.js
+++ js/myevent.js
@@ -1,19 +1,26 @@
-$(function(){
-    
+$(function () {
+
 });
 
-function goProject(pro){
-    if(pro == 1){
-        window.open("http://www.aiitone.com:20171/");
-    } else if(pro == 2){
-        window.open("https://github.com/Byube/deliveryAPI");
-    } else if(pro == 3){
-        window.open("https://github.com/Byube/Apiisfun");
-    } else if(pro == 4){
-        window.open("https://github.com/Byube/swa/tree/master/swa");
-    } else if (pro == 5){
-       alert("소스 공개 불가");
-    } else if (pro == 6){
-        window.open("https://www.grammarschool.ai/common/greeting.do");
+function goProject(pro) {
+    switch (pro) {
+        case 1:
+            window.open("http://www.aiitone.com:20171/");
+            break;
+        case 2:
+            window.open("https://github.com/Byube/deliveryAPI");
+            break;
+        case 3:
+            window.open("https://github.com/Byube/Apiisfun");
+            break;
+        case 4:
+            window.open("https://github.com/Byube/swa/tree/master/swa");
+            break;
+        case 6:
+            window.open("https://www.grammarschool.ai/common/greeting.do");
+            break;
+        default:
+            alert("소스 공개 불가");
+            break;
     }
 }
